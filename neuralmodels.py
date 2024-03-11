769bebe2a926377b8a0fa9975ee53a9618242209
Thijs Snelleman <snelleman@aim.rwth-aachen.de>
2024-03-11 14:01:40 +0100

    NCI1 fix

--- neuralmodels.py
+++ neuralmodels.py
@@ -545,16 +545,16 @@ class GraphConvPoolNNIMDBMulti(torch.nn.Module):
 
 class GraphConvPoolNNNCI1(torch.nn.Module):
     archName = "GCN NCI1"
-    def __init__(self, node_features, task_type_node, num_classes, dataset_name, device):
+    def __init__(self, node_features, task_type_node, num_classes, datset_name, device):
         super().__init__()
-        self.n_epochs = 200
+        self.n_epochs = 2000
         self.num_classes = num_classes
         self.device = device
         self.poolLayer = ClusterPooling
         self.hid_channel = 32
         self.batch_size = 1
         self.learningrate = 0.01
-        self.lrhalving = True
+        self.lrhalving = False
         self.halvinginterval = 50
         
         if self.num_classes == 2: #binary
@@ -569,12 +569,15 @@ class GraphConvPoolNNNCI1(torch.nn.Module):
         self.conv1 = GINConv(torch.nn.Linear(node_features, self.hid_channel))
         #self.conv2 = GCNConv(self.hid_channel, self.hid_channel)
         self.conv2 = GINConv(torch.nn.Linear(self.hid_channel, self.hid_channel))
-        self.pool1 = self.poolLayer(self.hid_channel, dropout=dropout_pool)
+        #self.pool1 = self.poolLayer(self.hid_channel, dropout=dropout_pool)
         
-        self.conv3 = GCNConv(self.hid_channel, self.hid_channel)
-        self.conv4 = GCNConv(self.hid_channel, self.hid_channel)
-        self.pool2 = self.poolLayer(self.hid_channel, dropout=dropout_pool)
-        self.conv5 = GCNConv(self.hid_channel, self.hid_channel)
+        #self.conv3 = GCNConv(self.hid_channel, self.hid_channel)
+        self.conv3 = GINConv(torch.nn.Linear(self.hid_channel, self.hid_channel))
+        #self.conv4 = GCNConv(self.hid_channel, self.hid_channel)
+        self.conv4 = GINConv(torch.nn.Linear(self.hid_channel, self.hid_channel))
+        #self.pool2 = self.poolLayer(self.hid_channel, dropout=dropout_pool)
+        #self.conv5 = GCNConv(self.hid_channel, self.hid_channel)
+        self.conv5 = GINConv(torch.nn.Linear(self.hid_channel, self.hid_channel))
 
         self.fc1 = torch.nn.Linear(self.hid_channel, self.hid_channel)
         self.fc2 = torch.nn.Linear(self.hid_channel, self.num_classes)
@@ -584,7 +587,6 @@ class GraphConvPoolNNNCI1(torch.nn.Module):
         data = Data(x=data[0], edge_index=data[1].t().contiguous())
 
         x, edge_index = data.x, data.edge_index
-        
         x = self.conv1(x, edge_index)
         x = F.relu(x)
         x = self.dropout(x)
@@ -593,7 +595,7 @@ class GraphConvPoolNNNCI1(torch.nn.Module):
         x = F.relu(x)
         x = self.dropout(x)
 
-        x, edge_index, batch, unpool1 = self.pool1(x, edge_index.long(), batch)
+        #x, edge_index, batch, unpool1 = self.pool1(x, edge_index.long(), batch)
 
         x = self.conv3(x, edge_index)
         x = F.relu(x)
@@ -603,32 +605,22 @@ class GraphConvPoolNNNCI1(torch.nn.Module):
         x = F.relu(x)
         x = self.dropout(x)
 
-        x, edge_index, batch, unpool2 = self.pool2(x, edge_index.long(), batch)
+        #x, edge_index, batch, unpool2 = self.pool2(x, edge_index.long(), batch)
 
         x = self.conv5(x, edge_index)
         x = F.relu(x)
         x = self.dropout(x)
 
-        if self.task_type_node: #Dealing with node classification
-            x, edge_index, batch = self.pool2.unpool(x, unpool2)
-            x, edge_index, batch = self.pool1.unpool(x, unpool1)
-        else: #dealing with graph classification
-            x = global_mean_pool(x, batch)
-            #Try global sum pool
-            #Maybe try global max pool maar dat verpest misschien de gradients
+        x = global_mean_pool(x, batch)
 
         x = self.fc1(x)
         x = F.relu(x)
         x = self.dropout(x)
         x = self.fc2(x)
         
+        x = torch.sigmoid(x)
         
-        if self.num_classes == 1: #binary
-            x = torch.sigmoid(x)
-            return torch.flatten(x)
-        else:
-            return torch.nn.functional.log_softmax(x, dim=1)
-            # return torch.nn.functional.softmax(x, dim=1)
+        return torch.flatten(x)
 
         
 class GraphConvPoolNN(torch.nn.Module):
