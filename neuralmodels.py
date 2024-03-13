d212a4a0aa77c75f3f454fb96015adbefe08beab
Thijs Snelleman <snelleman@aim.rwth-aachen.de>
2024-03-13 14:38:30 +0100

    Fix for rerun xu collab

--- neuralmodels.py
+++ neuralmodels.py
@@ -647,9 +647,16 @@ class GCNModel(ModelInterface):
                 if self.n_labels == 2:
                     output_dim = 1
                 readout = "sum"
-                if self.dataset_name != "PROTEIN" and  self.dataset_name != "NCI1":
+                if self.dataset_name != "PROTEIN" and  self.dataset_name != "NCI1" or self.dataset_name != "COLLAB":
                     readout = "average"
-                self.clf = xugcn.GraphCNN(num_layers=5, num_mlp_layers=2, input_dim=self.n_node_features, hidden_dim=hidden_dim, output_dim=output_dim, final_dropout=0.5, learn_eps=False, graph_pooling_type=readout, neighbor_pooling_type="sum", device=self.device)
+                drop_factor = 0.5
+
+                self.clf = xugcn.GraphCNN(num_layers=5, num_mlp_layers=2,
+                                          input_dim=self.n_node_features,
+                                          hidden_dim=hidden_dim,
+                                          output_dim=output_dim,
+                                          final_dropout=drop_factor, learn_eps=False,
+                                          graph_pooling_type=readout, neighbor_pooling_type="sum", device=self.device)
                 self.clf.batch_size = 128
                 if self.dataset_name == "PROTEIN" or self.dataset_name == "REDDIT-BINARY":
                     self.clf.batch_size = 32
