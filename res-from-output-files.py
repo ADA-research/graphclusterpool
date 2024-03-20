1ad45a2fd13a503b90f3ea558fd7c3d927de4e9a
Thijs Snelleman <snelleman@aim.rwth-aachen.de>
2024-03-20 09:21:45 +0100

    added in visualization for paper, changed how to extract results

--- res-from-output-files.py
+++ res-from-output-files.py
@@ -8,12 +8,19 @@ dir = sys.argv[1]
 files = [x for x in os.listdir(dir) if x.endswith(".out")]
 
 res = []
-
+res_name = []
 for file in files:
     line = open(dir + "/" + file, 'r').readlines()[-1]
     if line.strip().startswith("[!]"):
         res.append(float(line.split(" ")[-1]))
+        res_name.append(file)
 
 print(f"[{len(res)}] Result: {np.mean(res)} +/ {np.std(res)}")
-res.sort()
-print(res)
+
+#res.sort()
+if len(sys.argv) > 2:
+    for f, r in zip(res_name, res):
+        print(r, '\t', f)
+else:
+    res.sort()
+    print(res)
